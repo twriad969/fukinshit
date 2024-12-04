1ee3ab05fa8ae2f1ed2aeb549ac9dab44e1b29e7
twriad969 <157061362+twriad969@users.noreply.github.com>
2024-12-05 03:13:45 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient, events
-from quart import Quart, request, jsonify
+from quart import Quart, request, jsonify, make_response
 import asyncio
 import time
 from collections import deque
@@ -11,14 +11,13 @@ phone_number = '+8801790423900'
 
 # Quart app (async version of Flask)
 app = Quart(__name__)
-app.config['PROVIDE_AUTOMATIC_OPTIONS'] = True
 
-# Enable CORS
-@app.after_request
-async def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+@app.route('/', methods=['OPTIONS'])
+async def handle_options():
+    response = await make_response()
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
 # Global Telegram Client
@@ -89,13 +88,16 @@ def clean_old_links():
 
 @app.route('/')
 async def send_link():
+    # Add CORS headers to the response
+    response = await make_response(jsonify({"error": "No link provided!"}), 400) if not request.args.get('link') else None
+    
     global processed_links_today
 
     # Get the link from the query parameters
     link = request.args.get('link')
 
     if not link:
-        return jsonify({"error": "No link provided!"}), 400
+        return response
 
     # Reset daily limit if a new day has started
     if time.time() - daily_reset_timestamp > ONE_DAY:
@@ -106,7 +108,9 @@ async def send_link():
 
     # Check if either the 30-minute or daily limit has been exceeded
     if len(processed_links_last_30_minutes) >= MAX_LINKS_30_MINUTES or processed_links_today >= MAX_LINKS_PER_DAY:
-        return jsonify({"response": link})  # Return the original link if limits are exceeded
+        response = await make_response(jsonify({"response": link}))
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
 
     # Run the Telegram client interaction asynchronously
     bot_response = await interact_with_bot(link)
@@ -128,8 +132,10 @@ async def send_link():
     processed_links_last_30_minutes.append(time.time())  # Record the current timestamp
     processed_links_today += 1  # Increment the daily counter
 
-    # Return the bot's response as JSON
-    return jsonify({"response": bot_response})
+    # Return the bot's response as JSON with CORS headers
+    response = await make_response(jsonify({"response": bot_response}))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == '__main__':
     # Run the Quart app using Uvicorn for async support
